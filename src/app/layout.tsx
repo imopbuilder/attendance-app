5ec12924a5403cd4d3e7d218804661871f6b11b0
imopbuilder <opbuilder9@gmail.com>
2024-01-15 10:09:06 +0530

    fix: resolve supress-hydration-warning due to next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<body className={`font-sans ${inter.variable}`}>
 				<ClerkProvider>
 					<TRPCReactProvider cookies={cookies().toString()}>
