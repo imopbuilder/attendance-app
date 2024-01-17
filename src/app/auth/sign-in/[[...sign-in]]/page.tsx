4a0d6b114914022f56a0de3a608a36c2790b3a21
imopbuilder <opbuilder9@gmail.com>
2024-01-17 16:10:49 +0530

    feat: add auth pages

---
+++ src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,20 @@
+import Footer from '@/components/global/footer';
+import AuthHeader from '@/components/pages/auth';
+import { SignIn } from '@clerk/nextjs';
+import { Fragment } from 'react';
+
+export default function page() {
+	return (
+		<Fragment>
+			<AuthHeader />
+			<main>
+				<section>
+					<div className='max-w-maxi mx-auto flex items-center justify-center py-14 min-h-hvh'>
+						<SignIn />
+					</div>
+				</section>
+			</main>
+			<Footer />
+		</Fragment>
+	);
+}
