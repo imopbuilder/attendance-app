4f2bb28b1e3a816a76ff9621d7385fe890750bac
imopbuilder <opbuilder9@gmail.com>
2024-01-17 18:38:36 +0530

    feat: change margin in footer and auth-header

--- src/components/pages/auth/index.tsx
+++ src/components/pages/auth/index.tsx
@@ -4,8 +4,8 @@ import { ThemeAnimate } from './client';
 
 export default function AuthHeader() {
 	return (
-		<header className='border-b px-[4%]'>
-			<div className='max-w-maxi mx-auto h-16 flex items-center justify-between'>
+		<header className='border-b'>
+			<div className='mx-5 h-16 flex items-center justify-between'>
 				<div className='flex items-center justify-center'>
 					<Link href={'/'} className='font-semibold'>
 						Attandence App
