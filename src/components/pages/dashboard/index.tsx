cbf62cc30679920dc5235eebde0226e33bdc183d
imopbuilder <opbuilder9@gmail.com>
2024-01-20 11:46:55 +0530

    feat: add subjects loading component in dashboard route

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -1,19 +1,56 @@
 import { DoughnutChart } from '@/components/global/chart/doughnut-chart';
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 import { cn } from '@/lib/utils/cn';
 import { db } from '@/server/db';
 import { Subject, subjects } from '@/server/db/schema/subject';
 import { SignedInAuthObject } from '@clerk/nextjs/server';
 import { eq } from 'drizzle-orm';
-import { Pencil, Settings } from 'lucide-react';
+import { Pencil, Settings, Trash2 } from 'lucide-react';
 import { Fragment } from 'react';
 import { DeleteSubjectBtn } from './client';
 
 export function SubjectsLoader() {
 	return (
-		<div>
-			<p>loading...</p>
-		</div>
+		<Fragment>
+			{[1, 2, 3, 4, 5].map((val) => (
+				<div key={val} className='p-4 rounded-lg border mb-4'>
+					<div className='flex items-start justify-between mb-4 gap-5'>
+						<div className='w-full'>
+							<h2 className='font-medium capitalize'>
+								<Skeleton className='w-full h-6' />
+							</h2>
+							<Skeleton className='w-full h-5 mt-1 mb-2 max-w-40' />
+							<div className='mt-2'>
+								<Skeleton className='w-full h-6 max-w-40' />
+							</div>
+						</div>
+						<div className='w-full max-w-[100px] aspect-square'>
+							<Skeleton className='w-full aspect-square' />
+						</div>
+					</div>
+					<hr />
+					<div className='mt-3 flex items-end justify-between'>
+						<div className='space-x-2 pl-1.5'>
+							{'NNNNN'
+								.toLowerCase()
+								.split('')
+								.map((_, index) => (
+									<span key={`${index}`} className={cn('size-2.5 inline-block rounded-full relative -top-0.5', 'bg-muted-foreground')} />
+								))}
+						</div>
+						<div>
+							<Button className='p-1.5 h-auto w-auto ml-auto mr-1 group' variant='ghost' size='icon'>
+								<Pencil size={16} className='text-muted-foreground group-hover:text-foreground duration-200' />
+							</Button>
+							<Button className={'p-1.5 h-auto w-auto group hover:bg-destructive'} variant='ghost' size='icon' disabled={true}>
+								<Trash2 size={16} className='text-muted-foreground group-hover:text-destructive-foreground duration-200' />
+							</Button>
+						</div>
+					</div>
+				</div>
+			))}
+		</Fragment>
 	);
 }
 
