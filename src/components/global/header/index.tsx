6d9af578a29fb1a0bcc3d5562ab028fa6383d06f
imopbuilder <opbuilder9@gmail.com>
2024-01-22 12:09:04 +0530

    styles: change padding in the sign-in and sign-up buttons in header component

--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -16,14 +16,14 @@ export default function Header() {
 				<nav className='flex items-center justify-center sm:gap-4 gap-3'>
 					<ThemeToggle />
 					<SignedOut>
-						<ul className='flex items-center justify-center gap-4'>
+						<ul className='flex items-center justify-center sm:gap-4 gap-3'>
 							<li>
-								<Button asChild variant='secondary' size='lg'>
+								<Button className='p-4 sm:px-8' variant='secondary' size='lg' asChild>
 									<Link href='/auth/sign-in'>Sign in</Link>
 								</Button>
 							</li>
 							<li>
-								<Button asChild size='lg'>
+								<Button className='p-4 sm:px-8' size='lg' asChild>
 									<Link href='/auth/sign-up'>Sign up</Link>
 								</Button>
 							</li>
